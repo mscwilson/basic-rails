cb8f408e71ff20a8c6f84086f2e100cf0241790a
Miranda Wilson <miranda@snowplowanalytics.com>
2021-07-12 17:23:05 +0100

    Change method of displaying tooltip

--- app/javascript/tooltip.js
+++ app/javascript/tooltip.js
@@ -1,9 +1,16 @@
-function showColourTooltip(event){
-  const positiveWords = ["great choice", "great choice", "lovely", "nice", "excellent", "love it", "beautiful"]
-  const randomPositiveWords = positiveWords[Math.floor(Math.random() * positiveWords.length)]
+function showColourTooltip(event, position){
+  // console.log(position);
 
-  const colour = event.target.className.split(" ")[1]
-  const message = `${colour[0].toUpperCase()}${colour.slice(1)}, ${randomPositiveWords}!`
+  const colourName = event.target.className.split(" ")[1]
+  const message = `${colourName[0].toUpperCase()}${colourName.slice(1)}, ${nicePhrase()}!`
+
+  const inserted = document.createElement("div");
+  document.body.appendChild(inserted)
+  inserted.className = "tooltiptext2"
+  inserted.style.cssText = "position:absolute; color: red; backgroundColor: black; z-position: 1;";
+  inserted.innerHTML = `<span style= "visibility: visible">${message}</span>`
+  inserted.style.left = position.left + "px";
+  inserted.style.top = position.top + "px";
 
   const tooltip = document.querySelectorAll('.tooltip')[0];
   const tooltiptext = document.querySelectorAll('.tooltiptext')[0];
@@ -15,17 +22,26 @@ function showColourTooltip(event){
   tooltiptext.style.visibility = 'visible';
 }
 
+function nicePhrase() {
+  const positiveWords = ["great choice", "great choice", "lovely", "nice", "excellent", "love it", "beautiful"]
+  return positiveWords[Math.floor(Math.random() * positiveWords.length)]
+}
+
 function hideTooltip() {
   let tooltip = document.querySelectorAll('.tooltiptext')[0];
   tooltip.style.visibility = 'hidden';
+
+  let newTooltip = document.getElementsByClassName("tooltiptext2")
+  // newTooltip.remove()
 }
 
 document.addEventListener("turbolinks:load", function() {
 
   const squares = document.querySelectorAll("div.colour");
   squares.forEach((element) => {
+    const position = element.getBoundingClientRect();
     element.addEventListener("click", (event) => {
-      showColourTooltip(event);
+      showColourTooltip(event, position);
     })
     element.addEventListener("mouseleave", hideTooltip);
   })
--- spec/features/looking_at_colours_spec.rb
+++ spec/features/looking_at_colours_spec.rb
@@ -4,7 +4,8 @@ RSpec.feature "displaying colours", type: :feature do
 
   scenario "on index page" do
     visit "/colours/index"
-    expect(page).to have_text "What's your favourite colour today?"
-
+    expect(page).to have_text "What's your favourite colour right now?"
   end
+
+  scenario "clicking on a colour"
 end
