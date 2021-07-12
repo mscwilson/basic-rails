698545cf1b414ce8bec71bb17ea11c3cdf329684
Miranda Wilson <miranda@snowplowanalytics.com>
2021-07-12 12:57:12 +0100

    Show colour message in tooltip

--- app/javascript/tooltip.js
+++ app/javascript/tooltip.js
@@ -2,15 +2,24 @@ document.addEventListener("turbolinks:load", function() {
 
   console.log("here in tooltip.js")
 
-  function buttonClicked(event){
+  function showColourTooltip(event){
     console.log("here in buttonClicked")
     console.log(event);
+
+    const positiveWords = ["great choice", "great choice", "lovely", "nice", "excellent", "love it", "beautiful"]
+    const randomPositiveWords = positiveWords[Math.floor(Math.random() * positiveWords.length)]
+
+    const colour = event.target.className.split(" ")[1]
+    console.log(colour);
+    const message = `${colour[0].toUpperCase()}${colour.slice(1)}, ${randomPositiveWords}!`
 
     const tooltip = document.querySelectorAll('.tooltip')[0];
     const tooltiptext = document.querySelectorAll('.tooltiptext')[0];
 
     tooltip.style.top = (event.pageY - 20) + "px";
     tooltip.style.left = (event.pageX - 60) + "px";
+
+    tooltiptext.innerHTML = message
     tooltiptext.style.visibility = 'visible';
   }
 
@@ -20,19 +29,20 @@ document.addEventListener("turbolinks:load", function() {
     tooltip.style.visibility = 'hidden';
   }
 
-  // document.getElementById('button1').addEventListener("click", buttonClicked);
-  // document.getElementById('button1').addEventListener("mouseout", hideTooltip);
+    function printMessage() {
+      console.log("left the grid");
+    }
 
   document.getElementsByClassName("container")[0].onclick = function(event) {
     const target = event.target;
     if (!target.classList.contains("colour")) return;
     console.log("a colour got clicked")
 
-    target.addEventListener("click", buttonClicked(event));
-    target.addEventListener("mouseout", hideTooltip);
+    target.addEventListener("click", showColourTooltip(event));
 
   };
 
+  document.getElementsByClassName("grid")[0].addEventListener("mouseleave", printMessage);
 
 
 
